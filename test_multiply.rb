405fd31e3c8a6d364e36ca11e82bd5e0c224649c
Chad Swatsworth <Chad Swatsworth>
2017-01-23 11:13:34 -0500

    multiply

--- test_multiply.rb
+++ test_multiply.rb
@@ -1,11 +1,18 @@
 require 'minitest/autorun'
 require_relative 'multiply.rb'
 
-class TestMMFunctions < Minitest::TestM
+class TestMMFunctions < Minitest::Test
 	
 	def test_assert_1_equals_1
-		assert_equals(1,1)
+		assert_equal(1,1)
 	end
 
 	def test_2_times_3_returns_six
-		
+		assert_equal(6,multiply(2,3))
+	end
+
+	def test_3_times_4_returns_12
+		assert_equal(12,multiply(3,4))
+	end
+
+end
