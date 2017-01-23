01b785a6ec4d849d2225459141a68503d8303eb4
Chad Swatsworth <Chad Swatsworth>
2017-01-23 12:04:09 -0500

    passing multiple arguments

--- test_add.rb
+++ test_add.rb
@@ -7,7 +7,7 @@ class TestMMFunctions < Minitest::Test
 	end
 	
 	def test_functions_one_plus_one_equals_two
-		assert_equal(2, add(1,1))
+		assert_equal(2,add(1,1))
 	end
 
 	def test_add_2_plus_3_returns_five
@@ -15,11 +15,15 @@ class TestMMFunctions < Minitest::Test
 	end				#puts (2,3)
 
 	def test_add_20_plus_one_returns_21
-		assert_equal(21, add(20,1))
+		assert_equal(21,add(20,1))
 	end
 
 	def test_add_10_plus_negative_10_returns_zero
-		assert_equal(0, add(10,-10))
+		assert_equal(0,add(10,-10))
 	end
 	
+	def test_add_20_plus_5_plus_5_returns_thirty
+		assert_equal(30,add(20,5,5))
+	end
 end
+
